d681d80d403fe75c72acea2047ad0191e8e1b121
guilleeh <guilleeh@uci.edu>
2019-11-12 22:25:53 -0800

    user authentication complete

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -7,16 +7,63 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    
 
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        self.loadingIndicator.startAnimating()
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil {
+                self.loadingIndicator.stopAnimating()
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                self.loadingIndicator.stopAnimating()
+                print("\(error?.localizedDescription ?? "Unknown Error")")
+                let alertController = UIAlertController(title: "Sign up Error", message:
+                    "Something went wrong when loggin in :(", preferredStyle: .alert)
+                
+                alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        self.loadingIndicator.startAnimating()
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.loadingIndicator.stopAnimating()
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                self.loadingIndicator.stopAnimating()
+                print("\(error?.localizedDescription ?? "Unknown Error")")
+                let alertController = UIAlertController(title: "Sign up Error", message:
+                    "Something went wrong when signing you up :(", preferredStyle: .alert)
+                
+                alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+    }
     /*
     // MARK: - Navigation
 
