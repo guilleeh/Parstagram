f9bf4b8871f4c67c922d821ef0c0443b9724a8fd
guilleeh <guilleeh@uci.edu>
2019-11-19 16:18:18 -0800

    User stays logged in

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -12,8 +12,6 @@ import Parse
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
@@ -24,7 +22,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 configuration.server = "https://guarded-sea-32735.herokuapp.com/parse"
             })
         )
-        
         
         return true
     }
